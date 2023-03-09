e6bf286e19d930f5bc8003adbd5ea5673d58fcbe
shenjy <shenjy24@gmail.com>
2023-03-09 11:48:51 +0800

    接入公众号

--- src/main/java/com/jonas/ai/controller/WechatController.java
+++ src/main/java/com/jonas/ai/controller/WechatController.java
@@ -2,16 +2,17 @@ package com.jonas.ai.controller;
 
 import com.jonas.ai.bean.req.PublicAccountReq;
 import com.jonas.ai.config.DirectReturn;
+import com.jonas.ai.config.model.BizException;
+import com.jonas.ai.config.model.SystemCode;
 import com.jonas.ai.service.WechatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/wechat")
+@RequestMapping("/wechat/public/account/callback")
 public class WechatController {
 
     private final WechatService wechatService;
@@ -23,11 +24,30 @@ public class WechatController {
      * @return
      */
     @DirectReturn
-    @RequestMapping("/public/account/callback")
+    @GetMapping(produces = "text/plain;charset=utf-8")
     public String callback(PublicAccountReq req) {
         if (wechatService.checkPublicAccountSignature(req)) {
             return req.getEchostr();
         }
         return "";
+    }
+
+    @DirectReturn
+    @PostMapping(produces = "application/xml;charset=utf-8")
+    public String receiveFromWechat(@RequestBody String requestBody,
+                          @RequestParam("signature") String signature,
+                          @RequestParam("timestamp") String timestamp,
+                          @RequestParam("nonce") String nonce,
+                          @RequestParam("openid") String openid,
+                          @RequestParam(name = "encrypt_type", required = false) String encType,
+                          @RequestParam(name = "msg_signature", required = false) String msgSignature) {
+        log.info("\n接收微信请求：[openid=[{}], [signature=[{}], encType=[{}], msgSignature=[{}],"
+                        + " timestamp=[{}], nonce=[{}], requestBody=[\n{}\n] ",
+                openid, signature, encType, msgSignature, timestamp, nonce, requestBody);
+        if (wechatService.checkPublicAccountSignature(timestamp, nonce, signature)) {
+            log.error("验签失败");
+            throw new BizException(SystemCode.BIZ_ERROR);
+        }
+        return wechatService.receiveFromWechat(openid, requestBody, encType, msgSignature);
     }
 }
--- src/main/java/com/jonas/ai/service/WechatService.java
+++ src/main/java/com/jonas/ai/service/WechatService.java
@@ -3,8 +3,12 @@ package com.jonas.ai.service;
 import com.jonas.ai.bean.req.PublicAccountReq;
 import com.jonas.ai.config.model.BizException;
 import com.jonas.ai.config.model.ErrorCode;
+import com.jonas.ai.util.GsonUtil;
 import com.jonas.ai.util.StringUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +18,7 @@ import java.util.Arrays;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class WechatService {
 
     @Value("${wechat.public.account.token}")
@@ -23,8 +28,12 @@ public class WechatService {
      * 校验公众号回调接口的签名
      */
     public boolean checkPublicAccountSignature(PublicAccountReq req) {
+        return checkPublicAccountSignature(req.getTimestamp(), req.getNonce(), req.getSignature());
+    }
+
+    public boolean checkPublicAccountSignature(String timestamp, String nonce, String signature) {
         // 1.将token、timestamp、nonce三个参数进行字典序排序
-        String[] arr = new String[]{publicAccountToken, req.getTimestamp(), req.getNonce()};
+        String[] arr = new String[]{publicAccountToken, timestamp, nonce};
         Arrays.sort(arr);
 
         // 2. 将三个参数字符串拼接成一个字符串进行sha1加密
@@ -36,10 +45,45 @@ public class WechatService {
             MessageDigest md = MessageDigest.getInstance("SHA-1");
             byte[] digest = md.digest(content.toString().getBytes());
             String tmpStr = StringUtil.byteToStr(digest);
-            return req.getSignature().equalsIgnoreCase(tmpStr);
+            return signature.equalsIgnoreCase(tmpStr);
         } catch (NoSuchAlgorithmException e) {
             log.error("checkPublicAccountSignature error", e);
             throw new BizException(ErrorCode.REQUEST_ERROR);
         }
+    }
+
+    public String receiveFromWechat(String openid, String requestBody, String encType, String msgSignature) {
+        String out = "";
+        if (null == encType) {
+            // 明文传输
+            WxMpXmlMessage inMessage = WxMpXmlMessage.fromXml(requestBody);
+            WxMpXmlOutMessage outMessage = this.route(inMessage);
+            if (null == outMessage) {
+                return "";
+            }
+            out = outMessage.toXml();
+        }
+        log.info("\n组装回复信息：{}", out);
+        return out;
+    }
+
+    private WxMpXmlOutMessage route(WxMpXmlMessage wxMessage) {
+        try {
+
+            //TODO 组装回复消息
+            String content = "收到信息内容：" + GsonUtil.toJson(wxMessage);
+
+            return build(content, wxMessage);
+        } catch (Exception e) {
+            log.error("路由消息时出现异常！", e);
+        }
+        return null;
+    }
+
+    private WxMpXmlOutMessage build(String content, WxMpXmlMessage wxMessage) {
+        return WxMpXmlOutMessage.TEXT().content(content)
+                .fromUser(wxMessage.getToUser())
+                .toUser(wxMessage.getFromUser())
+                .build();
     }
 }
